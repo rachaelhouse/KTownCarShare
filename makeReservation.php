5da827f08b868fc940d0cfe1816302d7bf6c1938
rachaelHouse <12rh39@queensu.ca>
2017-03-27 17:11:33 -0400

    Added redirect after reservation

--- makeReservation.php
+++ makeReservation.php
@@ -11,15 +11,18 @@ $length=$_POST['Length'];
 
 $Rno = rand(1111,9999);
 if(mysqli_query($cxn,"INSERT INTO reservation values ('$Rno', '$date', $length, '$member_ID','$VIN')")){
-echo "<h1 align='center'> The Booking was successful!";
+echo "<h1 align='center'> The Booking was successful! Redirtecting to homepage in 5 seconds";
+header('Refresh: 5; URL=index.php');
 
 }else{
-	echo "<h1 align='center'> The Booking was NOT successful. ";
-	
+	echo "<h1 align='center'> The Booking was NOT successful. Redirtecting to Reservation page in 5 seconds ";
+	header('Refresh: 5; URL=createReservation.php');
 }
 }else{
-	echo "<h1 align='center'> The Booking was NOT successful. ";
+	echo "<h1 align='center'> The Booking was NOT successful. Redirtecting to Reservation in 5 seconds";
+	header('Refresh: 5; URL=createReservation.php');
 
 }
+
 
 ?>
