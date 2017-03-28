d751ce1609f34fb8bc5b1946c38d3c3a1a5c6970
Rhys Eylott <12re6@queensu.ca>
2017-03-28 18:48:02 -0400

    spacing

--- showCars.php
+++ showCars.php
@@ -3,13 +3,13 @@ include_once('connect.php');
 
 $date=date("Y-m-d");
 
-echo "Cars Available on ", $date;
+echo "Cars Available on ", $date, "<br>";
 
 $unCars = (mysqli_query($cxn,"SELECT VIN FROM Reservation WHERE RDate = $date"));
 $cars = (mysqli_query($cxn,"SELECT * FROM Car "));
 if (mysqli_num_rows($cars) > 0) {
 while ($row = mysqli_fetch_assoc($cars)){
-	echo "VIN: ", $row['VIN'], "<br> Make: ", $row['Make'], " Model: ", $row['Model'], " Year: ", $row['Year'] ," Location: ", $row['Location'], " Fee: ", $row['Fee'];
+	echo "<br>VIN: ", $row['VIN'], "\tMake: ", $row['Make'], "\tModel: ", $row['Model'], "\tYear: ", $row['Year'] ,"\tLocation: ", $row['Location'], "\tFee: ", $row['Fee'], "<br>";
 
 }
 }
