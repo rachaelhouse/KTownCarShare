5170f437f6283033722acdc7fe88f97f79a72a47
Rhys Eylott <12re6@queensu.ca>
2017-03-28 18:38:35 -0400

    inserted a break

--- showCars.php
+++ showCars.php
@@ -9,7 +9,7 @@ $unCars = (mysqli_query($cxn,"SELECT VIN FROM Reservation WHERE RDate = $date"))
 $cars = (mysqli_query($cxn,"SELECT * FROM Car "));
 if (mysqli_num_rows($cars) > 0) {
 while ($row = mysqli_fetch_assoc($cars)){
-	echo "VIN: ", $row['VIN'], " Make: ", $row['Make'], " Model: ", $row['Model'], " Year: ", $row['Year'] ," Location: ", $row['Location'], " Fee: ", $row['Fee'];
+	echo "VIN: ", $row['VIN'], "<br> Make: ", $row['Make'], " Model: ", $row['Model'], " Year: ", $row['Year'] ," Location: ", $row['Location'], " Fee: ", $row['Fee'];
 
 }
 }
