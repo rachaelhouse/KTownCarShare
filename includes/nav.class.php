168b349b0d06953802f4220ad95b987a63781524
Rhys Eylott <12re6@queensu.ca>
2017-03-29 14:41:51 -0400

    making it look guuuud

--- includes/nav.class.php
+++ includes/nav.class.php
@@ -1,4 +1,5 @@
 <nav class="navbar navbar-default navbar-static-top" id="navbar_freshsight">
+  <body style="background-color: #819bb2; border: 1px grey; margin: 20px;">
   <div class="container-fluid">
         <!-- Logo Images -->
         <a class="navbar-brand" href="./index.php">
@@ -8,13 +9,13 @@
     <!-- Nav Bar -->  
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li class=""><a href="./registration.php" class="navbar-item" style="">Register</a></li>
-        <li class=""><a href="./reservation.php" class="navbar-item" style="">Reservation</a></li>      
-        <li class=""><a href="./location.php" class="navbar-item" style="">Location</a></li>
-        <li class=""><a href="./comment.php" class="navbar-item" style="">Comments</a></li>
-         <li class=""><a href="./login.php" class="navbar-item" style="">Login</a></li>
-         <li class=""><a href="./cars.php" class="navbar-item" style="">Available Cars</a></li>
-         <li class=""><a href="./admin/index.php" class="navbar-item" style="">Go To Admin</a></li>
+        <li class=""><a href="./registration.php" class="navbar-item" style="background-color: #dcdfe1;">Register</a></li>
+        <li class=""><a href="./reservation.php" class="navbar-item" style="background-color: #dcdfe1;">Reservation</a></li>      
+        <li class=""><a href="./location.php" class="navbar-item" style="background-color: #dcdfe1;">Location</a></li>
+        <li class=""><a href="./comment.php" class="navbar-item" style="background-color: #dcdfe1;">Comments</a></li>
+         <li class=""><a href="./login.php" class="navbar-item" style="background-color: #dcdfe1;">Login</a></li>
+         <li class=""><a href="./cars.php" class="navbar-item" style="background-color: #dcdfe1;">Available Cars</a></li>
+         <li class=""><a href="./admin/index.php" class="navbar-item" style="background-color: #dcdfe1;">Go To Admin</a></li>
       </ul>
 
     </div><!-- End Nav Bar-->
