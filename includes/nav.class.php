3ca7072579a1a536df0a38a32d7587837c2ae1e6
rachaelHouse <12rh39@queensu.ca>
2017-03-29 14:42:38 -0400

    changing admin

--- includes/nav.class.php
+++ includes/nav.class.php
@@ -2,7 +2,11 @@
   <div class="container-fluid">
         <!-- Logo Images -->
         <a class="navbar-brand" href="./index.php">
+<<<<<<< Updated upstream
             <img src="./assets/images/homebtn.png?" style="height:500%;">
+=======
+            <img src="./assets/images/house.png?" style="height:500%;">
+>>>>>>> Stashed changes
         </a> 
 
     <!-- Nav Bar -->  
@@ -14,7 +18,7 @@
         <li class=""><a href="./comment.php" class="navbar-item" style="">Comments</a></li>
          <li class=""><a href="./login.php" class="navbar-item" style="">Login</a></li>
          <li class=""><a href="./cars.php" class="navbar-item" style="">Available Cars</a></li>
-         <li class=""><a href="./admin/index.php" class="navbar-item" style="">Go To Admin</a></li>
+         <li class=""><a href="./admin/index.php" class="navbar-item" style="">Go TO Admin</a></li>
       </ul>
 
     </div><!-- End Nav Bar-->
