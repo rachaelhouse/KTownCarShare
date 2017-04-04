4b08fbaedb175a31431c0e17b7ab120414c59b8c
Rhys Eylott <12re6@queensu.ca>
2017-04-03 21:58:16 -0400

    tried deleting VIN after comment

--- admin/feedback.php
+++ admin/feedback.php
@@ -18,10 +18,19 @@ echo "<h1 align = 'center'><br>Comments Needing Feedback";
 $result = (mysqli_query($cxn,"SELECT Comment, MemID, VIN, Rating FROM Comment"));
 while ($row = mysqli_fetch_assoc($result)){
 	echo "<h4 align = 'center'><br>Member: ", $row['MemID'],"<br> VIN: ", $row['VIN'],"<br> Comment: ", $row['Comment'],"<br> Rating: ", $row['Rating'], "<br>";	
-	echo "<form action='feedbackPage.php'>";
+	echo "<form action='feedback.php'>";
 	echo "<textarea name='comment' rows='7' cols='50'> </textarea>";
-	echo "<input type='submit' value='Submit'>";
+	echo "<input type='submit' name='id' value='Submit'>";
+	$VIN1 = $_GET['VIN'];
+	$sql = "DELETE FROM 'Comment' where 'VIN' = $VIN1";
+	if(isset($_POST['id']))
+	{
+		$VIN1 = $_GET['VIN'];
+ 		$sql = "DELETE FROM 'Comment' where 'VIN' = $VIN1"; 
+ 		echo"hi";
+	}
 	echo "</form>";
+
 }
 ?>
 
